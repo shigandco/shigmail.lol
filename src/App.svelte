8f0ba12dbf5a5acf11a2fa06a08abd7a56e6d88e
exhq <exhq@shigmail.lol>
2023-10-09 16:33:13 +0330

    second commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,20 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import shiggy from "./assets/shiggy.png"
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
+  
+  <img src="" alt="shiggy logo" style="display: inline-block; vertical-align: middle;">
+  <h1 style="display: inline-block; vertical-align: middle;">shigmail</h1>
+  <h2>cutest mail service</h2>
   <div class="card">
-    <Counter />
+
   </div>
 
   <p>
     Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
   </p>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
@@ -34,6 +23,8 @@
     padding: 1.5em;
     will-change: filter;
     transition: filter 300ms;
+    display: inline-block;
+    vertical-align: middle;
   }
   .logo:hover {
     filter: drop-shadow(0 0 2em #646cffaa);
